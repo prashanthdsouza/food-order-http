4b5e9b5cb54d65a086d82fb40d4a6c9615aabf80
prashanthdsouza <79136584+prashanthdsouza@users.noreply.github.com>
2022-09-10 11:03:27 +0530

    218

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -8,11 +8,16 @@ import classes from './AvailableMeals.module.css';
 const AvailableMeals = () => {
   const [meals,setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () =>{
       const response = await fetch('https://react-http-2f0d6-default-rtdb.firebaseio.com/meals.json').then();
       const responseData = await response.json();
+
+      if (!response.ok) {
+        throw new Error ('Something went wrong.');
+      }
 
       const loadedMeals = [];
 
@@ -29,13 +34,23 @@ const AvailableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals();
+
+        fetchMeals().catch(error => {
+          setIsLoading (false);
+          setHttpError(error.message);
+        });
   }, []);
 
   if (isLoading) {
     return <section className={classes.MealsLoading}>
       <p>Loading...</p>
     </section>
+  }
+
+  if (httpError) {
+    return <section className={classes.MealsError}>
+    <p>{httpError}</p>
+  </section>
   }
 
   const mealsList = meals.map((meal) => (
